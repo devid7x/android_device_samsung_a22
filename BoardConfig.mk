ee06712757d506d85d254cd7a48b66502e30263b
devid7x <123992077+devid7x@users.noreply.github.com>
2024-07-28 10:42:50 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,13 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/xiaomi/lancelot
+DEVICE_PATH := device/samsung/a22
 
 # Inherit from mt6768-common
 -include device/xiaomi/mt6768-common/BoardConfigCommon.mk
 
 # Asserts
-TARGET_OTA_ASSERT_DEVICE := lancelot,galahad,shiva
+TARGET_OTA_ASSERT_DEVICE := a22,a22nsxx
 
 # Display
 TARGET_SCREEN_DENSITY := 440
