c473fc08838c9d61db6825a432ef9a9ba6bfcd18
devid7x <123992077+devid7x@users.noreply.github.com>
2024-07-28 20:35:49 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lancelot.mk
+    $(LOCAL_DIR)/lineage_a22.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lancelot-user \
-    lineage_lancelot-userdebug \
-    lineage_lancelot-eng
+    lineage_a22-user \
+    lineage_a22-userdebug \
+    lineage_a22-eng
