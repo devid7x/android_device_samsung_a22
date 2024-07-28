40b7eff3f85cc91c25f34bf6915a2a57c096d989
devid7x <123992077+devid7x@users.noreply.github.com>
2024-07-28 20:29:29 +0300

    Update lineage_lancelot.mk

--- lineage_lancelot.mk
+++ lineage_lancelot.mk
@@ -9,16 +9,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device makefile.
-$(call inherit-product, device/xiaomi/lancelot/device.mk)
+$(call inherit-product, device/samsung/a22/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lancelot
-PRODUCT_DEVICE := lancelot
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi 9
+PRODUCT_NAME := lineage_a22
+PRODUCT_DEVICE := a22
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_BRAND := Galaxy
+PRODUCT_MODEL := Galaxy A22
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
